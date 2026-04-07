4bff166a79b08b42c6ebd8ead81e11a6f4461644
Stefan Schneider <eqsoft4@gmail.com>
2026-04-07 20:24:55 +0200

    add output folder

--- run.sh
+++ run.sh
@@ -6,14 +6,20 @@ export CIBUILDER_BIN_URL="$2"
 export CIBUILDER_BIN_REF="$3"
 IMAGE="$4"
 
+CIBUILD_OUTPUT="${RUNNER_TEMP:-/tmp}/cibuild-output"
+mkdir -p "${CIBUILD_OUTPUT}"
+chmod 1777 "${CIBUILD_OUTPUT}"
+
 env | grep '^GITHUB_' > github.env
 env | grep '^ACTIONS_' >> github.env
 env | grep '^CIBUILD_' >> github.env
 env | grep '^CIBUILDER_' >> github.env
+echo "CIBUILD_OUTPUT_DIR=/cibuild-output" >> github.env
 
 docker run --privileged --rm \
   --env-file github.env \
   -v "$PWD:/workspace" \
+  -v "${CIBUILD_OUTPUT}:/cibuild-output" \
   -w /workspace \
   "$IMAGE"
 
